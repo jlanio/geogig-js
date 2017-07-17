138b3556838de1dc7ea57ba200500e535b978711
jlanio <lanio.morais@gmail.com>
2017-07-17 08:33:46 -0500

    Initial Commit

---
+++ geogig-js/controllers/GeogigCtrl.js
@@ -0,0 +1,26 @@
+class Geogig {
+    static init(){
+    }
+    static importShapefile(){
+    }
+    static add(){
+    }
+    static commit (comment){
+    }
+    static analyze(){
+    }
+    static log(){
+    }
+    static diffCommit(thisCommit, toCompare) {
+    }
+    static ConnectRemote (remoteObj , dataObj){
+    }
+    static updateRemoteRepositories(id, dataObj){
+    }
+    static clone (){
+    }
+    static pull (){
+    }
+    static push (){
+    }
+}
