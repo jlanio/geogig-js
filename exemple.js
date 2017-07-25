9cc1a0b0cf0e3658457f1824d23cbab4a56f9fa6
jlanio <lanio.morais@gmail.com>
2017-07-25 12:09:48 -0500

    update rename folder to /lib

--- exemple.js
+++ exemple.js
@@ -1,30 +1,30 @@
 'use strict';
 
-const geogigJS = require('./geogig-js/models/Geogig');
+const geogigJS = require('./bin/models/Geogig');
 // INITIAL CONFIG
 let geogig = new geogigJS({
   bin: "C:\\Users\\jlanio\\Geogig-Desktop\\app\\static\\geogig\\bin\\geogig.bat",
   cwd: "C:\\Users\\jlanio\\Geogig-Desktop\\app\\static\\tmp"
 });
 // START SERVE
-geogig.serve.init().then(e => console.log(e))
+// geogig.serve.init().then(e => console.log(e))
 
 // CONFIG connect to Serve
-// let localhost = geogig.serve.connect({uri: 'http://localhost:8182/repos'})
+let localhost = geogig.serve.connect({uri: 'http://localhost:8182/repos'})
 
 //INIT REPOSITORY IN LOCAL PATCH
 // let newRepo = geogig.repo({name:'Amazonas2'});
 // newRepo.init.then(e => console.log(e))
 
-let Amazonas = localhost.repos.findOne({name: 'Amazonas'});
-Amazonas.then(get => get.log()).then(e => console.log(e))
-Amazonas.then(get => get.geopackage.import({
-    fileUpload: 'C:\\Users\\jlanio\\Desktop\\GeoPackage\\imovel.gpkg',
-    transactionId: 'e89175f5-6a9c-497d-968a-eb42221226e9',
-    interchange: true,
-    message: 'Ola Mundo'
-  })
-).then(e => console.log(e))
+// let Amazonas = localhost.repos.findOne({name: 'Amazonas'});
+// Amazonas.then(get => get.log()).then(e => console.log(e));
+// Amazonas.then(get => get.geopackage.import({
+//     fileUpload: 'C:\\Users\\jlanio\\Desktop\\GeoPackage\\imovel.gpkg',
+//     transactionId: 'e89175f5-6a9c-497d-968a-eb42221226e9',
+//     interchange: true,
+//     message: 'Ola Mundo'
+//   })
+// ).then(e => console.log(e))
 // Amazonas.then(get => get.commit)
 // Amazonas.then(get => get.beginTransaction)
 // Amazonas.then(get => get.endTransaction(
--- main.js
+++ main.js
@@ -5,4 +5,12 @@
  * Released under the GPL-3.0 License.
  */
 
-'use strict';
+ 'use strict';
+
+ const geogigJS = require('./geogig-js/models/Geogig');
+
+ let geogig = new geogigJS({
+   bin: "C:\\Users\\jlanio\\Geogig-Desktop\\app\\static\\geogig\\bin\\geogig.bat",
+   cwd: "C:\\Users\\jlanio\\Geogig-Desktop\\app\\static\\tmp"
+ });
+console.log(geogig);
